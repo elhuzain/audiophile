c8e1566d975c136bf32c051215c39ac68a740a2a
elhuzain <a.huzain98@gmail.com>
2026-09-16 23:29:13 +0300

    style: styled buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,31 +2,19 @@ import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-    "group/button inline-flex shrink-0 items-center justify-center rounded-none border border-transparent bg-clip-padding text-xs font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    "group/button inline-flex shrink-0 font-bold uppercase gap-1 items-center justify-center rounded-none border border-transparent bg-clip-padding text-xs whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
     {
         variants: {
             variant: {
-                default: "bg-primary text-white hover:bg-primary/80",
+                default: "bg-primary h-12 px-8 tracking-[1px] text-white hover:bg-primary/80",
                 secondary:
-                    "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+                    "bg-white text-black border tracking-[1px] border-black h-12 px-8 hover:bg-black hover:text-white",
                 ghost:
-                    "bg-transparent p-0! h-fit! text-black/50 uppercase text-[13px] tracking-[1px] hover:text-primary",
-            },
-            size: {
-                default:
-                    "h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-                xs: "h-6 gap-1 rounded-none px-2 text-xs has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
-                sm: "h-7 gap-1 rounded-none px-2.5 has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
-                lg: "h-9 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-                icon: "size-8",
-                "icon-xs": "size-6 rounded-none [&_svg:not([class*='size-'])]:size-3",
-                "icon-sm": "size-7 rounded-none",
-                "icon-lg": "size-9",
+                    "bg-transparent p-0! h-fit! text-black/50 text-[13px] tracking-[1px] hover:text-primary",
             },
         },
         defaultVariants: {
             variant: "default",
-            size: "default",
         },
     }
 )
